401c95642a54e760f44e6df7fb635fbc22edf657
e.cheremisin <empat1@mail.ru>
2025-12-04 02:25:03 +0300

    create right swipe

--- app/src/main/java/ru/empat/morewords/presentation/learn/WordScreen.kt
+++ app/src/main/java/ru/empat/morewords/presentation/learn/WordScreen.kt
@@ -1,40 +1,49 @@
 package ru.empat.morewords.presentation.learn
 
+import android.util.Log
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.snap
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissBoxValue.EndToStart
-import androidx.compose.material3.SwipeToDismissBoxValue.Settled
-import androidx.compose.material3.SwipeToDismissBoxValue.StartToEnd
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import ru.empat.morewords.R
+import androidx.compose.ui.zIndex
 import ru.empat.morewords.domain.entity.Word
-import ru.empat.morewords.ui.theme.Green
-import ru.empat.morewords.ui.theme.Red
+import kotlin.math.abs
+import kotlin.random.Random
 
 
 const val ROTATION = 180f
+const val ALPHA_ANIMATION_DURATION = 400
+const val ROTATION_ANIMATION_DURATION = 400
+const val SWIPE_ANIMATION_DURATION = 1000
+
+const val DETECT_DRAG_LONG = 100
 
 @Composable
 fun LearnCardContent(component: LearnCardComponent) {
@@ -45,26 +54,68 @@ fun LearnCardContent(component: LearnCardComponent) {
         LearnCardStore.State.WordState.Init -> {}
         LearnCardStore.State.WordState.Loading -> {}
         is LearnCardStore.State.WordState.WordLoaded -> {
-
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                MyCard(
-                    word = currentState.word,
-                    hide = currentState.isHide,
-                    onClick = {
-                        component.onClick()
-                    },
-                    onRight = {
-                        component.learn(currentState.word.wordId , true)
-                    },
-                    onLeft = {
-                        component.learn(currentState.word.wordId , false)
-                    }
-                )
-            }
+            LoadedWord(component, currentState)
         }
+    }
+}
+
+@Composable
+private fun LoadedWord(
+    component: LearnCardComponent,
+    state: LearnCardStore.State.WordState.WordLoaded
+) {
+    val animationStep: MutableState<Animation> =
+        remember(state.word) { mutableStateOf(Animation.Init) }
+
+    val wordId = state.word.wordId
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        MyCard(
+            word = state.word,
+            hide = state.isHide,
+            onClick = {
+                component.onClick()
+            },
+            animationStep = animationStep.value,
+            onRight = {
+                animationStep.value = Animation.Right.Swipe
+            },
+            onLeft = {
+                component.learn(wordId, false)
+            },
+            animationEnd = {
+                when (it) {
+                    Animation.Init -> {}
+                    Animation.Left.SwipeLeft -> TODO()
+                    Animation.Left.SwipeRight -> {
+                        component.learn(wordId, false)
+                        animationStep.value = Animation.Init
+                    }
+
+                    Animation.Right.Swipe -> {
+                        component.learn(wordId, true)
+                        animationStep.value = Animation.Init
+                    }
+                }
+            }
+        )
+        BackgroundCards()
+    }
+}
+
+@Composable
+fun BackgroundCards() {
+    repeat(5) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(0.7f)
+                .fillMaxHeight(0.55f)
+                .zIndex(5f)
+                .rotate(Random.nextInt(from = -10, until = 10).toFloat())
+        ) {}
     }
 }
 
@@ -72,104 +123,160 @@ fun LearnCardContent(component: LearnCardComponent) {
 fun MyCard(
     word: Word,
     hide: Boolean,
+    animationStep: Animation,
     onClick: (() -> Unit),
     onRight: (() -> Unit),
-    onLeft: (() -> Unit)
+    onLeft: (() -> Unit),
+    animationEnd: (Animation) -> Unit
 ) {
 
-    val swipeToDismissBoxState = rememberSwipeToDismissBoxState(
-        confirmValueChange = {
-            if (it == StartToEnd) onRight.invoke()
-            else if (it == EndToStart) onLeft.invoke()
-            it != StartToEnd
-        }
-    )
+    Log.d("Animation", "step ${animationStep}")
+
+    val isInit = animationStep is Animation.Init
+
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp
+    val swipeLength = screenWidth.toFloat() * 2
+
+
+    var endOffsetX by remember(word) { mutableFloatStateOf(0f) }
+    var endOffsetY by remember(word) { mutableFloatStateOf(0f) }
 
     val stateRotationY by animateFloatAsState(
         targetValue = if (hide) 180f else 0f,
-        animationSpec = tween(durationMillis = 1000)
+        animationSpec = if (hide) snap() else tween(durationMillis = ROTATION_ANIMATION_DURATION)
     )
 
-    SwipeToDismissBox(
-        state = swipeToDismissBoxState,
-        enableDismissFromStartToEnd = !hide,
-        enableDismissFromEndToStart = !hide,
-        modifier = Modifier
-            .padding(16.dp),
-        backgroundContent = {
-            when (swipeToDismissBoxState.dismissDirection) {
-                StartToEnd -> {
-                    Side(stringResource(R.string.know), Green, contentAlignment = Alignment.TopEnd)
-                }
+    val alphaAnimation by animateFloatAsState(
+        targetValue = if (animationStep is Animation.Right.Swipe) 0f else 1f,
+        animationSpec = if (isInit) tween(durationMillis = ALPHA_ANIMATION_DURATION) else snap()
+    )
 
-                EndToStart -> {
-                    Side(stringResource(R.string.notKnow), Red, Alignment.TopStart)
-                }
-
-                Settled -> {}
+    val rightSwipeAnimation by animateFloatAsState(
+        targetValue = if (animationStep is Animation.Right.Swipe) swipeLength else 0f,
+        animationSpec = if (isInit) snap() else tween(durationMillis = SWIPE_ANIMATION_DURATION),
+        finishedListener = {
+            if (animationStep is Animation.Right.Swipe) {
+                animationEnd.invoke(Animation.Right.Swipe)
             }
         }
-    ) {
-        Card(
-            modifier = Modifier
-                .size(320.dp, 480.dp)
-                .graphicsLayer {
-                    rotationY = stateRotationY
-                    cameraDistance = 12f * density
-                }
-                .clickable {
-                    onClick.invoke()
-                },
-            elevation = CardDefaults.cardElevation(
-                defaultElevation = 6.dp
-            )
-        ) {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .graphicsLayer {
-                        if (stateRotationY == ROTATION) {
-                            rotationY = ROTATION
+    )
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(0.70f)
+            .fillMaxHeight(0.55f)
+            .clickable {
+                if (animationStep.isAnimate) return@clickable
+
+                onClick.invoke()
+            }
+            .zIndex(10f)
+            .graphicsLayer {
+                translationX = endOffsetX + rightSwipeAnimation
+                translationY = endOffsetY
+                rotationY = stateRotationY
+                cameraDistance = 12f * density
+                alpha = alphaAnimation
+            }
+            .pointerInput(hide) {
+                detectDragGestures(
+                    onDragEnd = {
+                        when (endDragAnimation(endOffsetX)) {
+                            EndDragSwipe.Left -> {
+                                onLeft()
+                            }
+
+                            EndDragSwipe.Right -> {
+                                onRight()
+                            }
+
+                            EndDragSwipe.No -> {
+                                endOffsetX = 0f
+                                endOffsetY = 0f
+                            }
                         }
+                    },
+                    onDrag = { change, dragAmount ->
+                        if (!hide && !animationStep.isAnimate) {
+                            endOffsetX += dragAmount.x
+                            endOffsetY += dragAmount.y
+                        }
+                        change.consume()
                     }
-            ) {
+                )
+            },
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        )
+    ) {
+        WordCard(stateRotationY, word)
+    }
+}
+
+@Stable
+sealed class Animation(val isAnimate: Boolean) {
+
+    data object Init : Animation(false)
+    sealed class Right(isAnimate: Boolean) : Animation(isAnimate) {
+        data object Swipe : Right(true)
+    }
+
+    sealed class Left(isAnimate: Boolean) : Animation(isAnimate) {
+        data object SwipeLeft : Right(true)
+        data object SwipeRight : Right(true)
+    }
+}
+
+@Composable
+fun WordCard(stateRotationY: Float, word: Word) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .graphicsLayer {
                 if (stateRotationY == ROTATION) {
-                    Text(
-                        text = word.translate,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                } else {
-                    Column {
-                        Text(
-                            text = word.text,
-                            textAlign = TextAlign.Center,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .graphicsLayer {
-                                    rotationY = stateRotationY
-                                    cameraDistance = 12f * density
-                                }
-                        )
-                    }
+                    rotationY = ROTATION
                 }
+            }
+    ) {
+        if (stateRotationY == ROTATION) {
+            Text(
+                text = word.translate,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        } else {
+            Column {
+                Text(
+                    text = word.text,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .graphicsLayer {
+                            rotationY = stateRotationY
+                            cameraDistance = 12f * density
+                        }
+                )
             }
         }
     }
 }
 
-@Composable
-fun Side(text: String, color: Color, contentAlignment: Alignment) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = contentAlignment
-    ) {
-        Text(
-            text = text,
-            color = color
-        )
+fun endDragAnimation(endOffsetX: Float): EndDragSwipe {
+    if (abs(endOffsetX) < DETECT_DRAG_LONG)
+        return EndDragSwipe.No
+
+    return if (endOffsetX > 0) {
+        EndDragSwipe.Right
+    } else {
+        EndDragSwipe.Left
     }
+}
+
+sealed interface EndDragSwipe {
+    data object Right : EndDragSwipe
+    data object Left : EndDragSwipe
+    data object No : EndDragSwipe
 }
